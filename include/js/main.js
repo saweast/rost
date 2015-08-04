6db0b199602723eb0643cc3b47474150e7ac9461
Vadim Lavorchuk <lavorchukvadim@yandex.ua>
2015-08-04 14:35:01 +0300

    add scrollTo

--- include/js/main.js
+++ include/js/main.js
@@ -2,15 +2,33 @@ $(document).ready(function() {
   var owl = $("#owl-demo");
   owl.owlCarousel({
       itemsCustom : [
-        [0, 2],
-        [450, 4],
-        [600, 4],
-        [700, 4],
-        [1000, 4],
-        [1200, 4],
-        [1400, 4],
-        [1600, 4]
+        [0, 4],
       ],
       navigation : true
   });
+
+  var SmoothScroll = (function() {
+    'use strict';
+    var $root = $('html, body');
+        /**
+         * Smooth scroll to element
+         * @param  {jQuery DOM element} $target
+         * @param  {Number}             xOffset (optional)
+         */
+    function smoothScroll($target, xOffset) {
+      var offset = xOffset || 0;
+      $root.animate({
+          scrollTop: $target.offset().top - offset - 30
+      }, 1000);
+    }
+      // Add scroll to links
+    $('nav.main-nav a').click(function(e) {
+      e.preventDefault();
+      smoothScroll($($.attr(this, 'href')));
+    });
+        // Public API
+    return {
+      to: smoothScroll
+    };
+  })();
 });
